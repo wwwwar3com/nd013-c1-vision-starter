9c650ba52d97fbc514467929b0ef2072e0a5873b
Frank <wwwwar3com@gmail.com>
2022-03-28 19:43:12 -0700

    Adding create_split impl

--- create_splits.py
+++ create_splits.py
@@ -1,9 +1,6 @@
 import argparse
-import glob
 import os
-import random
-
-import numpy as np
+import shutil
 
 from utils import get_module_logger
 
@@ -17,14 +14,22 @@ def split(source, destination):
         - source [str]: source data directory, contains the processed tf records
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
-    # TODO: Implement function
+    for idx, f_name in enumerate(os.listdir(source)):
+        if not f_name.endswith('tfrecord'):
+            continue
+        if idx % 100 < 15:
+            shutil.move(source + '/' + f_name, destination+'/test/'+f_name)
+        elif idx % 100 < 30:
+            shutil.move(source + '/' + f_name, destination+'/train/'+f_name)
+        else:
+            shutil.move(source + '/' + f_name, destination+'/val/'+f_name)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
-    parser.add_argument('--source', required=True,
+    parser.add_argument('--source', default='./data/waymo/training_and_validation',
                         help='source data directory')
-    parser.add_argument('--destination', required=True,
+    parser.add_argument('--destination', default='./data/waymo/',
                         help='destination data directory')
     args = parser.parse_args()
 
